8d87e5d6e56f64cbb68179070df26e17049987b5
sahil-2526 <sa400919@gmail.com>
2024-01-04 13:07:11 +0530

    added

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,35 +2,27 @@
 window.addEventListener("scroll", function()    {
     var header = this.document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY)
-})
+});
 
 
 //For homepage ad
 var i = 0;
-var images = [];
-
-images[0] = "https://images.unsplash.com/photo-1434030216411-0b793f4b4173?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-images[1] = "https://images.unsplash.com/photo-1535982330050-f1c2fb79ff78?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-images[2] = "https://plus.unsplash.com/premium_photo-1671069848075-c9faefe4df18?q=80&w=1373&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-images[3] = "https://images.unsplash.com/photo-1546410531-bb4caa6b424d?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-
-function changeImg(){
-    document.slide.src = images[i];
-    if(i < images.length - 1){
-        i++; 
-	}
-    else { 
-        i = 0;
-    }
-    setTimeout("changeImg()", 4000);
+var images = document.querySelectorAll('.ad .slide');
+function changeImg() {
+    images[i].style.display = 'none';
+    i = (i + 1) % images.length;
+    images[i].style.display = 'block';
+    setTimeout(changeImg, 4000);
 }
-
-const goPrev = ()=>{
-    i--;
-    changeImg();
+function goPrev() {
+    images[i].style.display = 'none';
+    i = (i - 1 + images.length) % images.length;
+    images[i].style.display = 'block';
 }
-const goNext = ()=>{
-    changeImg();
+function goNext() {
+    images[i].style.display = 'none';
+    i = (i + 1) % images.length;
+    images[i].style.display = 'block';
 }
 window.onload = changeImg;
 
@@ -46,4 +38,11 @@ document.querySelectorAll('.slider .box').forEach(box => {
 });
 document.querySelector('.popup span').onclick = () => {
     document.querySelector('.popup').style.display = 'none';
+}
+
+
+//contact us
+function hide(){
+    var menuBox = document.querySelector('header .menu-box');
+    menuBox.style.right = '-100vw'
 }
